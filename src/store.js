b58add7ccd3e212084680d70829f979f08ba5edc
zhangzq9527 <1641376001@qq.com>
2022-01-07 14:26:35 +0800

    study

--- src/store.js
+++ src/store.js
@@ -1,23 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Login from './modules/Login'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  state: {
-    user: {
-      username: '',
-      password: ''
-    }
-  },
-  mutations: {
-    login(state, payload) {
-      state.user.username = JSON.parse(payload).user
-      state.user.password = JSON.parse(payload).password
-    },
-    loginOut(state) {
-      state.user.username = ''
-    }
+  modules: {
+    Login
   }
 })
 
